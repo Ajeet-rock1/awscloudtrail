fad8f54c4bfdc0135cd9986474acb5f80f620f99
Ajeet <Ajeet-rock1@users.noreply.github.com>
2017-04-04 11:11:57 +0530

    Update delta.sh

--- delta.sh
+++ delta.sh
@@ -2,11 +2,11 @@
 year=`date +%Y`
 month=`date +%m`
 day=`date +%d`
-filetopull=`/usr/local/bin/s3cmd ls  s3://jc-pd-ct-001/AWSLogs/832198632263/CloudTrail/ap-south-1/$year/$month/$day/* |  tail -1 | awk '{print $4}'`
+filetopull=`/usr/local/bin/s3cmd ls  s3://delta/AWSLogs/856975675963/CloudTrail/us-east-1/$year/$month/$day/* |  tail -1 | awk '{print $4}'`
 fileforgunzip=`basename $filetopull`
 finalfile=`basename $filetopull .gz`
 /usr/local/bin/s3cmd get  $filetopull
 gunzip -f $fileforgunzip
-cat $finalfile  | /usr/bin/jq -r -M .Records[] -c >> /var/log/cloudtrail/Jiocloud.json
+cat $finalfile  | /usr/bin/jq -r -M .Records[] -c >> /var/log/cloudtrail/Delta.json
 rm -f $finalfile
 
